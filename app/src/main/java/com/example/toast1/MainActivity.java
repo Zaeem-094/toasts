678bd3e1d6c1ddcf8ba2119fa95b3f9a4bd95a4f
Zaeem-094 <18101002-094@uskt.edu.pk>
2021-10-31 21:04:43 +0500

    toast1

---
+++ app/src/main/java/com/example/toast1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.toast1;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        setContentView(R.layout.main);
+        Toast.makeText(
+                getApplicationContext(),
+                "Saludos amigos \n Hasta la vista",
+                Toast.LENGTH_LONG).show();
+    }
+}
